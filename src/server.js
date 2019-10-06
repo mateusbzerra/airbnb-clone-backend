e6b859f189ebd0a42a4bfd8956876dcf3221039d
Mateus Bezerra <mateusbzerra@gmail.com>
2019-10-06 17:34:39 -0300

    Cors adicionado e configuração de campos virtual no model Spot

--- src/server.js
+++ src/server.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/omnistack9", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  "mongodb://localhost:27017/omnistack9",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  function(err) {
+    if (err) console.log("### error while conecting to database");
+  }
+);
 
+app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333);
